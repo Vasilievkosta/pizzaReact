25848c3986198e783300a9c848242c2ad9e1135a
Vasilievkosta <vasilievkosta@gmail.com>
2022-06-13 02:23:04 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+import './scss/app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	
+	const [items, setItems] = useState([]);
+	
+	React.useEffect(() => {
+		fetch('https://626d16545267c14d5677d9c2.mockapi.io/items')
+			.then((res)=> res.json())
+			.then((json)=> {
+				setItems(json);
+			});
+	}, []);	
+	
+	return (
+    <div className="wrapper">
+	
+		<Header />
+	  
+		<div className="content">
+			<div className="container">
+				<div className="content__top">
+				  
+					<Categories />
+					<Sort />
+					
+				</div>
+				  
+				<h2 className="content__title">Все пиццы</h2>
+				<div className="content__items">
+				{items.map((obj)=> (
+					<PizzaBlock key={obj.id} {...obj}/>
+				))}					
+					
+				</div>
+			</div>
+		</div>
     </div>
   );
 }
