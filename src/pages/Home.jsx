05ff457db885c3773912807b581782136e8407b8
Vasilievkosta <vasilievkosta@gmail.com>
2022-09-19 23:54:49 +0300

    added Pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,43 @@
 import React, { useState } from 'react';
+import { SearchContext } from '../App';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
 import Sceleton from '../components/PizzaBlock/Sceleton';
+import Pagination from '../components/Pagination';
 
 function Home() {
+    const { searchValue } = React.useContext(SearchContext);
 
     const [items, setItems] = useState([]);
     const [isLoading, setLoading] = useState(true);
     const [categoryId, setCategoryId] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const [sortType, setSortType] = useState({
         name: 'популярности', sortProperty: 'rating'
     });
 
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const sort = `${sortType.sortProperty}&order=desc`;
+    const search = searchValue ? `&search=${searchValue}` : '';
+
+
+
     React.useEffect(() => {
         setLoading(true);
-        fetch(`https://631b6309fae3df4dcffd7df6.mockapi.io/api/items?${categoryId > 0 ? `category=${categoryId}` : ''
-            }&sortBy=${sortType.sortProperty}&order=desc`)
+        fetch(`https://631b6309fae3df4dcffd7df6.mockapi.io/api/items?page=${currentPage}&limit=4${category}&sortBy=${sort}${search}`)
             .then((res) => res.json())
             .then((arr) => {
                 setItems(arr);
                 setLoading(false);
             });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType]);
+    }, [categoryId, sortType, searchValue, currentPage]);
+
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
+    const sceletons = [...new Array(6)].map((_, index) => <Sceleton key={index} />);
 
     return (
         <div className="container">
@@ -37,14 +50,12 @@ function Home() {
 
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-                {
-                    isLoading ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
-                        : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
-                }
-
+                {isLoading ? sceletons : pizzas}
             </div>
+            <Pagination onChangePage={(number) => setCurrentPage(number)} />
         </div>
     );
 }
+
 
 export default Home;
